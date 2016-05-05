656aa5c9178ec3cc9f6ca6d7dd59f0b7d76ec1d2
Diego Marcilio <dvmarcilio@gmail.com>
2016-05-05 17:17:31 -0300

    Using a command map to fetch a Validator from a Configuration

--- src/main/command/solution/refactored/RefactoredConfiguration.java
+++ src/main/command/solution/refactored/RefactoredConfiguration.java
@@ -2,25 +2,45 @@ package command.solution.refactored;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public enum RefactoredConfiguration {
 
-	LOG_LOGIN_FAILS(new BooleanConfigValueValidator()),
-	INSTITUTION_NAME(new StringValueValidator()),
-	TASKS_RETRY_INTERVAL(new IntConfigValueValidator());
+	LOG_LOGIN_FAILS(Boolean.class),
+	INSTITUTION_NAME(String.class),
+	TASKS_RETRY_INTERVAL(Integer.class);
 
-	private ConfigValueValidator validator;
+	private Class<?> valueType;
 
-	private RefactoredConfiguration(ConfigValueValidator validator) {
-		this.validator = validator;
-	}
-	
-	public ConfigValueValidator getValidator() {
-		return validator;
+	private static Map<Class<?>, ConfigValueValidator> configTypeToValidator = new HashMap<>();
+
+	private RefactoredConfiguration(Class<?> valueType) {
+		this.valueType = valueType;
 	}
 
 	public static Collection<RefactoredConfiguration> getAll() {
 		return Arrays.asList(RefactoredConfiguration.values());
+	}
+
+	public ConfigValueValidator getValidator() {
+		return fetchValidator(this);
+	}
+
+	private static ConfigValueValidator fetchValidator(
+			RefactoredConfiguration config) {
+		populateMap();
+		return configTypeToValidator.get(config.valueType);
+	}
+
+	private static void populateMap() {
+		if (configTypeToValidator.isEmpty()) {
+			configTypeToValidator.put(Integer.class,
+					new IntConfigValueValidator());
+			configTypeToValidator.put(Boolean.class,
+					new BooleanConfigValueValidator());
+			configTypeToValidator.put(String.class, new StringValueValidator());
+		}
 	}
 
 }
